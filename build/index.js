178012bd7a4d7b9ee31f476402ea2463b554f9dd
Hao Peng <phshy0607@126.com>
2019-01-10 17:10:41 +0800

    remove build

--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("http");
-var server_1 = require("./server");
-server_1.default.set('port', 3000);
-var server = http.createServer(server_1.default);
-server.listen(3000);
